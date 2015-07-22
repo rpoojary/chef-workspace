f1178a696ebc727a3998a2a1c5791212ace58c8d
root <root@rpoojary1.mylabserver.com>
2015-07-22 22:18:51 +0000

    Chef Repo Upload

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See https://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "rpoojary"
+client_key               "#{current_dir}/rpoojary.pem"
+validation_client_name   "ropo-validator"
+validation_key           "#{current_dir}/ropo-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/ropo"
+cookbook_path            ["#{current_dir}/../cookbooks"]
---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,10 @@
+default["apache"]["sites"]["rpoojary2"] = {"site_title" => "Ropo's world is coming soon", "port" => 80, "domain" => "rpoojary2.mylabserver.com"}
+default["apache"]["sites"]["rpoojary2b"] = {"port" => 80, "domain" => "rpoojary2b.mylabserver.com"}
+default["apache"]["sites"]["rpoojary3"] = {"site_title" => "Ropo's world is coming soon!!!", "port" => 80, "domain" => "rpoojary3.mylabserver.com"}
+
+case node["platform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end
